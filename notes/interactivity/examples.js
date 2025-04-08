9dfb9750bba74a4babf83a2300e24cff62b8fa21
nbriz <nickbriz@protonmail.com>
2025-04-08 11:49:34 -0500

    updates for class week3

---
+++ notes/interactivity/examples.js
@@ -0,0 +1,12 @@
+const examples = [
+  {
+    code: `<script src="https://cdn.jsdelivr.net/gh/netizenorg/netnet-standard-library/build/nn.min.js"></script>
+<body></body>
+<script>
+/* global nn */
+
+nn.create('span').content('😃').addTo('body')
+
+</script>`
+  }
+]
